217a841443b8e4e10f6f079f98934410b4df1a5f
canpolattest <107028235+canpolattest@users.noreply.github.com>
2022-10-19 10:10:15 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node 53!\n'
+  const msg = 'Hello Node 54!\n'
   res.end(msg);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on bir http://localhost:${port}/`);
+  console.log(`Server running on http://localhost:${port}/`);
 });
